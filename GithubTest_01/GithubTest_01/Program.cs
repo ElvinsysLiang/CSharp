fb8c170f9a215598d9611a1ac42ee4e8d61643be
ElvinsysLiang <elvinsys@gmail.com>
2020-01-08 20:11:44 +0800

    github test_01

---
+++ GithubTest_01/GithubTest_01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GithubTest_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first github test with CSharp");
+            Console.ReadKey();
+        }
+    }
+}
