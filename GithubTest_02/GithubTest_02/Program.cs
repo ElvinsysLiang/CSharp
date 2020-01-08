e894621e6d121b8394cd070e5dca6849cba34862
ElvinsysLiang <elvinsys@gmail.com>
2020-01-08 20:14:24 +0800

    github test 02

---
+++ GithubTest_02/GithubTest_02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GithubTest_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the second github test!");
+            Console.ReadKey();
+        }
+    }
+}
