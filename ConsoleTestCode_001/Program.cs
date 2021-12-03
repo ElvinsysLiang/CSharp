dcbb59906d437c52da3807bd3e96184e2ddbddd3
jianlong liang <jianlongliang@hotmail.com>
2021-12-03 10:24:43 +0900

    update test

---
+++ ConsoleTestCode_001/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestCode_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            //------------------------------
+            //------------------------------
+            //------------------------------
+            //------------------------------
+            Console.ReadKey();
+        }
+
+
+
+    }
+
+    public class Student
+    {
+        private string _name;
+        private int _age;
+        public Student() { }
+        public Student(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+    }
+}
---
+++ ConsoleTestCode_002/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleTestCode_002
+{
+    enum direct : byte
+    {
+        east=1,south,west,north
+    };
+
+    struct Route
+    {
+        public direct direction;
+        public double distance;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int direction = -1;
+            Route MyRoute;
+            Console.Write("select direction, 1.east 2.south 3.west 4.north:  ");
+            while (true)
+            {
+                try
+                {
+                    direction = Convert.ToInt32(Console.ReadLine());
+                    if (direction > 0 && direction < 5)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("select error, select 1-4:  ");
+                    }
+                }
+                catch
+                {
+                    Console.Write("input error, input 1-4:  ");
+                }
+            }
+            MyRoute.direction = (direct)direction;
+            Console.Write("input distance:  ");
+            while (true)
+            {
+                try
+                {
+                    MyRoute.distance = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.Write("error, input distance:  ");
+                }
+            }
+            Console.WriteLine($"MyRoute, direction is {MyRoute.direction}, distance is {MyRoute.distance}.");
+        }
+    }
+}
